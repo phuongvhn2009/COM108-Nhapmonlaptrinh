ab224be062a4a05dd9b364e833fddd5fcf64d442
Vu Hoang Nhat Phuong <phuongvhntb01940@gmail.com>
2025-10-24 15:35:25 +0700

    .

---
+++ lab3/bai2.c
@@ -0,0 +1,19 @@
+#include <math.h>
+#include <stdio.h>
+    int main(){
+        printf("nhap a ,b cho ham so bac nhat (a*x + b = 0): ");
+        float a, b;
+        scanf("%f%f", &a, &b);
+        if (a == 0){
+            if (b == 0){
+                printf("phuong trinh vo so nghiem");
+            }else{
+                printf("phuong trinh vo nghiem");
+            }
+        } 
+        else{
+            printf("phuong trinh co nghiem x = %.2f", -b/a);
+        }
+        return 0;
+
+    }
