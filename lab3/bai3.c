b78172c67eeeceef777afa154c4e8a8fc4fe401c
Vu Hoang Nhat Phuong <phuongvhntb01940@gmail.com>
2025-10-24 20:10:24 +0700

    .

--- lab3/bai3.c
+++ lab3/bai3.c
@@ -1,1 +1,32 @@
 #include <stdio.h>
+
+    int main() {
+        int a;
+        float tien;
+        printf("nhap so dien:");
+        scanf("%d",&a);
+        if (a <= 50) {
+            tien = a * 1678;
+            printf("so tien phai tra la: %.2f", tien);
+        }
+        else if (a <= 100) {
+            tien = a * 1734;
+            printf("so tien phai tra la: %.2f", tien);
+        }
+        else if (a <= 200) {
+            tien = a * 2014;
+            printf("so tien phai tra la: %.2f", tien);
+        }
+        else if (a <= 300) {
+            tien = a * 2536;
+            printf("so tien phai tra la: %.2f", tien);
+        }
+        else if (a <= 400) {
+            tien = a * 2.834;
+            printf("so tien phai tra la: %.2f", tien);
+        }
+        else {
+            tien = a * 2927;
+            printf("so tien phai tra la: %.2f", tien);
+        }
+    }
