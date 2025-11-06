e249abbcec4a5b2dfac035fc3e1d63cc8c90ef55
Vu Hoang Nhat Phuong <phuongvhntb01940@gmail.com>
2025-11-06 20:36:06 +0700

    Implement code changes to enhance functionality and improve performance

--- lab4/bai3.c
+++ lab4/bai3.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
 int main(){
+	int n,i;
+	scanf("%d",&n);
+	for (i=1;i<n;i++){
+		if(i*i==n){
+			printf("So chinh phuong");
+			return 0;
+		}
+	}
+	printf("Khong phai so chinh phuong");
 	return 0;
 }
