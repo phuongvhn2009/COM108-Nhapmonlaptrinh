dec06041c536ec30a8af753bfdbca82293999673
Vu Hoang Nhat Phuong <phuongvhntb01940@gmail.com>
2025-11-24 08:14:59 +0700

    Add initial gmon output file for profiling data

---
+++ lab6/bai1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+	
+	int n,i;
+	printf("Nhap so phan tu mang: ");
+	scanf("%d",&n);
+	int mang[n];
+	
+	for (i=0;i<n;i++){
+		printf("\na[%d]= ",i);
+		scanf("%d",&mang[i]);
+		}
+	
+	float tong,tb;
+	int dem=0;
+	
+	for (i=0;i<=n;i++){
+		if(mang[i] %3 ==0){
+			tong=tong+mang[i];
+			dem++;
+		}
+	}
+	tb=tong/dem;
+	printf("\ntrung binh cong cua mang la: %f",tb);
+	return 0;
+}
